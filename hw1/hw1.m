b9002398972fa7cb611e8b993798ec25952edd85
shiro019 <shiro019@umn.edu>
2023-09-18 14:07:05 +0800

    cleaner comments and reorganized folder

--- hw1/hw1.m
+++ hw1/hw1.m
@@ -1,14 +1,14 @@
 %% SET MODE! 
-% 
 % a=1   b=2   c=3
+MODE = 1;
 
-MODE = 2;
-
+% Define constants
 syms s
 a = 1;
 b = 2;
 c = 1;
 
+% Set up system vars
 if MODE == 1
     a_values = [a, a, a];
     b_values = [0.7*b, 1*b, 1.3*b];
@@ -31,6 +31,7 @@ elseif MODE == 3
     zeta_results = zeros(1, 3);
 end
 
+% Solve for wn and zeta
 for i = 1:3
     if MODE == 3
         term1 = s + zeta*wn_values(i) + wn_values(i)*sqrt(1-zeta^2)*1i;
@@ -47,33 +48,27 @@ for i = 1:3
     % Multiply the terms
     result = term1 * term2;
 
-    % Expand the resulting polynomial
+    % Expand resulting polynomial
     expanded_result = expand(result);
 
-    % Extract coefficients for x^1 and x^0
+    % Get coefficients for x^1 and x^0
     coeffs_result = coeffs(expanded_result);
     coeff_x1 = coeffs_result(2);
     coeff_x0 = coeffs_result(1);
 
-    % Calculate natural frequency (wn) and damping ratio (zeta)
+    % Calculate and store wn and zeta
     wn_results(i) = sqrt(coeff_x0);
     zeta_results(i) = coeff_x1 / (2 * wn_results(i));
 end
 
-% Display the results
-disp('Natural Frequencies (wn):');
-disp(wn_results);
-disp('Damping Ratios (zeta):');
-disp(zeta_results);
-
-% Create a time vector for simulation
+% Create a time vector for simulation and a figure for plotting
 t = 0:0.01:10;
-
-% Initialize a figure for plotting
 figure;
 
-% Loop through different values of wn, ζ
+% Loop through values of wn and zeta
 for i = 1:3
+
+    % Local vars
     zeta = zeta_results(i);
     wn = wn_results(i);
     K = c * wn;
@@ -91,14 +86,12 @@ for i = 1:3
     hold on;
 end
 
-% Add labels and legend
+% Format the figure
 xlabel('Time (s)');
 ylabel('Amplitude');
-title('Step Response of Second-Order System with Varying ζ');
+title('Step Response of Second-Order System');
 legend('Location', 'best');
 grid on;
-
-% Display the plot
 hold off;
 
 %% CHECK ANSWERS WITH LEC:  4-40/62
