d733f447d7af35a4b9d6b1ba9170a2f2b6caa9be
shiro019 <shiro019@umn.edu>
2023-10-24 18:49:28 +0800

    all parts completed

--- hw2/hw2.m
+++ hw2/hw2.m
@@ -2,12 +2,12 @@
 
 a = 1;
 b = 2;
-b_values = [0.05*b, 1*b, 50*b];  % Values of b to consider
+b_values = [0.05*b, 1*b, 50*b];
 
 figure;
 for i = 1:length(b_values)
     b = b_values(i);
-    G = tf(a, [1, b]);
+    G = tf(a, [1, b])
     
     % Calculate Bode plot
     [m, p, w] = bode(G);
@@ -26,7 +26,7 @@ for i = 1:length(b_values)
     hold on;
 end
 
-% Set labels and legends
+% Set labels and legends and show plot
 subplot(2, 1, 1);
 grid on;
 title('Bode Plot - Magnitude Response');
@@ -41,22 +41,18 @@ xlabel('Frequency (Hz)');
 ylabel('Phase (degrees)');
 legend('Location', 'Best');
 
-% Show the combined Bode plots
 sgtitle('Bode Plots for Different Values of b');
-
-% Adjust the figure size for better visibility
-set(gcf, 'Position', [100, 100, 800, 600]);
 
 %% B //----------//----------//----------//----------//----------//
 
 a = 1;
 b = 2;
-b_values = [0.05*b, 1*b, 50*b];  % Values of b to consider
+b_values = [0.05*b, 1*b, 50*b];
 
 figure;
 for i = 1:length(b_values)
     b = b_values(i);
-    G = tf(a, [1, b]);
+    G = tf(a, [1, b])
     
     % Calculate the Bode plot
     [m, p] = bode(G);
@@ -69,24 +65,19 @@ for i = 1:length(b_values)
     polarplot((p*pi/180), m, 'DisplayName', ['b = ' num2str(b)], 'LineWidth', 2);
     hold on;
 end
-
-% Set legend for polar plot
 legend('Location', 'Best');
-
-% Adjust the figure size for better visibility
-set(gcf, 'Position', [100, 100, 600, 600]);
 
 %% C //----------//----------//----------//----------//----------//
 
 a = 1;
 b = 2;
-c_values = [0.1*b, 10*b];  % Values of c to consider
+c_values = [0.1*b, 10*b];
 
 % Magnitude and phase plots
 figure;
 for i = 1:length(c_values)
     c = c_values(i);
-    G = tf([b, 1], [c, 1, 0, 0]);
+    G = tf([b, 1], [c, 1, 0, 0])
     
     % Calculate the Bode plot
     [m, p, w] = bode(G);
@@ -96,16 +87,16 @@ for i = 1:length(c_values)
     
     % Plot magnitude response
     subplot(2, 1, 1);
-    semilogx(f, 20*log10(squeeze(m)), 'DisplayName', ['c = ' num2str(c)]);
+    semilogx(f, 20*log10(squeeze(m)), 'DisplayName', ['c = ' num2str(c)], 'LineWidth', 2);
     hold on;
     
     % Plot phase response
     subplot(2, 1, 2);
-    semilogx(f, squeeze(p), 'DisplayName', ['c = ' num2str(c)]);
+    semilogx(f, squeeze(p), 'DisplayName', ['c = ' num2str(c)], 'LineWidth', 2);
     hold on;
 end
 
-% Set labels and legends for magnitude and phase plots
+% Set labels and legends and show plot
 subplot(2, 1, 1);
 grid on;
 title('Bode Plot - Magnitude Response');
@@ -120,7 +111,6 @@ xlabel('Frequency (Hz)');
 ylabel('Phase (degrees)');
 legend('Location', 'Best');
 
-% Show the combined Bode plots
 sgtitle('Bode Plots for Different Values of c');
 
 % Polar plot
@@ -132,21 +122,86 @@ for i = 1:length(c_values)
     % Calculate the Bode plot
     [m, p] = bode(G);
    
-    % Extract the magnitude and phase values for the first frequency point
+    % Make into 1D array
     m = m(1, :);
     p = p(1, :);
     
     % Generate the polar plot
-    polarplot((p* pi)/180, m, 'DisplayName', ['c = ' num2str(c)]);
+    polarplot((p* pi)/180, m, 'DisplayName', ['c = ' num2str(c)], 'LineWidth', 2);
     hold on;
 end
+legend('Location', 'Best');
 
 % Set the axis limits for the polar plot
 pax = gca;
 rlim([0 50]);
 
-% Set legend for polar plot
+%% D //----------//----------//----------//----------//----------//
+
+a = 1;
+b = 2;
+wn = b;
+zeta_values = [0.2, 0.5, 0.8];
+
+% Magnitude and phase plots
+figure;
+for i = 1:length(zeta_values)
+    zeta = zeta_values(i);
+    G = tf((a * wn^2), [1, 2 * zeta * wn, wn^2])
+    
+    % Calculate the Bode plot
+    [m, p, w] = bode(G);
+    
+    % Convert angular frequency to regular frequency (Hz)
+    f = w/(2*pi);
+    
+    % Plot magnitude response
+    subplot(2, 1, 1);
+    semilogx(f, 20*log10(squeeze(m)), 'DisplayName', ['ζ = ' num2str(zeta)], 'LineWidth', 2);
+    hold on;
+    
+    % Plot phase response
+    subplot(2, 1, 2);
+    semilogx(f, squeeze(p), 'DisplayName', ['ζ = ' num2str(zeta)], 'LineWidth', 2);
+    hold on;
+end
+
+% Set labels and legends and show plot
+subplot(2, 1, 1);
+grid on;
+title('Bode Plot - Magnitude Response');
+xlabel('Frequency (Hz)');
+ylabel('Magnitude (dB)');
 legend('Location', 'Best');
 
-% Adjust the figure size for better visibility
-set(gcf, 'Position', [100, 100, 600, 600]);
+subplot(2, 1, 2);
+grid on;
+title('Bode Plot - Phase Response');
+xlabel('Frequency (Hz)');
+ylabel('Phase (degrees)');
+legend('Location', 'Best');
+
+sgtitle('Bode Plots for Different Values of ζ');
+
+% Polar plot
+figure;
+for i = 1:length(zeta_values)
+    zeta = zeta_values(i);
+    G = tf((a * wn^2), [1, 2 * zeta * wn, wn^2]);
+    
+    % Calculate the Bode plot
+    [m, p] = bode(G);
+        
+    % Make into 1D array
+    m = m(1, :);
+    p = p(1, :);
+    
+    % Generate the polar plot
+    polarplot((p*pi)/180, m, 'DisplayName', ['ζ = ' num2str(zeta)], 'LineWidth', 2);
+    hold on;
+end
+legend('Location', 'Best');
+
+% Set the axis limits for the polar plot
+pax = gca;
+rlim([0 3]);  % Adjust the radius limit as needed
